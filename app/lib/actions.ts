660a4bee28b8a67ddcc6d03c6fde0ae85c861d3a
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2024-02-12 22:13:50 +0100

    chapter 12 - update/edit invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,3 +38,25 @@ export async function createInvoice(formData: FormData) {
   // For good measure we'll redirect the user back to `/dashboard/invoices` after the invoice has been created
   redirect('/dashboard/invoices');
 }
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
